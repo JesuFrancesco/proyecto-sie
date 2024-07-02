ffa01a8e3abfffc91f7d9fb242af718605ce9da0
Jesu <117495071+JesuFrancesco@users.noreply.github.com>
2024-07-02 00:18:19 -0500

    feat: data del 2023 todo: dimension domicilio

--- util/write_in_all_sheets.py
+++ util/write_in_all_sheets.py
@@ -7,12 +7,15 @@ def write_to_a1_in_all_sheets(file_path, text):
     # Iterate through each worksheet
     for sheet in workbook.worksheets:
         # Write the text to cell A1
-        sheet['A1'] = text
+        try:
+            sheet['B1'] = text
+        except:
+            continue
     
     # Save the workbook
     workbook.save(file_path)
 
 # Usage example
-file_path = "..\\static\\cleansing_laos_ventas_2024.xlsx"
-text = "N°"
+file_path = "..\\static\\cleansing_laos_ventas_2023.xlsx"
+text = ""
 write_to_a1_in_all_sheets(file_path, text)
